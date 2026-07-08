54bbf410094810f8a0cc1fd1bfa5fb2c5ce42079
Christer211 <109479907+Christer211@users.noreply.github.com>
2026-07-08 12:39:05 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,18 @@ set -e
 
 SDK=$(xcrun --sdk iphoneos --show-sdk-path)
 
-clang -dynamiclib -framework UIKit -framework Foundation -lobjc -arch arm64 -isysroot "$SDK" -miphoneos-version-min=13.0 -fobjc-arc -o RWFileManager.dylib RWFileManager.m
+clang -dynamiclib \
+  -framework UIKit \
+  -framework Foundation \
+  -framework CoreGraphics \
+  -framework QuartzCore \
+  -framework UniformTypeIdentifiers \
+  -lobjc \
+  -arch arm64 \
+  -isysroot "$SDK" \
+  -miphoneos-version-min=13.0 \
+  -fobjc-arc \
+  -o RWFileManager.dylib \
+  RWFileManager.m
 
 echo "Built RWFileManager.dylib"
